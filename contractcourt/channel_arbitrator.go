30e7412dea88cdec3d3c8648f6b02cd7f764c409
carla <kirkcohenc@gmail.com>
2020-10-30 16:23:54 +0200

    contractcourt: pass in optional txns to lookups required for arb startup
    To allow us to grab all of the information we need for our channel arbs
    in a more efficient way on startup, we add an optional tx to our lookup
    functions required on start.

--- contractcourt/channel_arbitrator.go
+++ contractcourt/channel_arbitrator.go
@@ -394,7 +394,7 @@ func (c *ChannelArbitrator) Start() error {
 
 	// First, we'll read our last state from disk, so our internal state
 	// machine can act accordingly.
-	c.state, err = c.log.CurrentState()
+	c.state, err = c.log.CurrentState(nil)
 	if err != nil {
 		return err
 	}
@@ -450,7 +450,7 @@ func (c *ChannelArbitrator) Start() error {
 	// older nodes, this won't be found at all, and will rely on the
 	// existing written chain actions. Additionally, if this channel hasn't
 	// logged any actions in the log, then this field won't be present.
-	commitSet, err := c.log.FetchConfirmedCommitSet()
+	commitSet, err := c.log.FetchConfirmedCommitSet(nil)
 	if err != nil && err != errNoCommitSet && err != errScopeBucketNoExist {
 		return err
 	}
